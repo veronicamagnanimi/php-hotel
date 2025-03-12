9ecb3e9d006d394cdd4381eedcab143622694ec9
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2025-03-12 16:52:02 +0100

    feat: table with bootstrap

--- index.php
+++ index.php
@@ -4,10 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
 
-<h1>Hotel</h1>
+<body>
+<div class="container">
+<h1 class="text-center my-3 fw-bold text-secondary">Hotel</h1>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>Nome</th>
+            <th>Descrizione</th>
+            <th>Parcheggio</th>
+            <th>Voto</th>
+            <th>Distanza dal centro</th>
+        </tr>
+    </thead>
+    <tbody>
+
+<!-- PHP -->
 <?php
 
     $hotels = [
@@ -54,11 +69,19 @@
 
 
     foreach ($hotels as $curItem) {
-        foreach ($curItem as $key => $value) {
-            echo $key . ": $value" . "<br>";
-        }
+        echo "<tr>";
+        echo "<td>{$curItem['name']}</td>";
+        echo "<td>{$curItem['description']}</td>";
+        echo "<td>" . ($curItem['parking'] ? 'Sì' : 'No') . "</td>";
+        echo "<td>{$curItem['vote']}</td>";
+        echo "<td>{$curItem['distance_to_center']} km</td>";
+        echo "</tr>";
     }
 
 ?>
+<!-- FINE PHP -->
+</tbody>
+</table>
+</div>
 </body>
 </html>
